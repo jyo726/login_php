2637a83380d6c37af77684193d16e4f458c64bf4
jyo726 <u03410109@ccu.edu.tw>
2021-11-14 18:02:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
-    exit;  //記得要跳出來，不然會重複轉址過多次
+    exit;  
 }
 ?>
 <html>
